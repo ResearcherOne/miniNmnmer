c31482cf716a8b72c41d812d050873e46e0ffe75
Birkan Kolcu <birkan.kolcu@ozu.edu.tr>
2019-12-06 07:25:29 -0700

    added graceful shutdown event registritation

--- src-pi/main.js
+++ src-pi/main.js
@@ -191,4 +191,26 @@ function initialize() {
     })
 }
 
+function exitHandler(options, exitCode) {
+    rpio.open(additionalPins.MQTT_CONNECTION_STATUS_PIN, rpio.OUTPUT, rpio.LOW);
+
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
+function registerGracefulShutdownEvents() {
+    process.stdin.resume();//so the program will not close instantly
+    //do something when app is closing
+    process.on('exit', exitHandler.bind(null,{cleanup:true}));
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+    // catches "kill pid" (for example: nodemon restart)
+    process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+    process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+}
+
 initialize();
+registerGracefulShutdownEvents();
